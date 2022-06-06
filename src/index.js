66f9126846fc0672839d52211f33acd21b54f6d9
Zhong <zhongzichen1997@163.com>
2022-06-06 09:41:03 +0800

    feat(index.js): :triangular_flag_on_post: 新增subscribe等调试案例

--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
 const { createStore, combineReducers, applyMiddleware, compose } = require('redux');
-const { counterReducer, todoReducer } = require('./reducer')
+const { counterReducer, todoReducer,counterAction , couterInitialState} = require('./reducer')
 
-const counterStore = createStore(counterReducer);
+// store enhancer 
+// const enhancer  = (...args)=>{
+//    console.debug("store enhancer: ",args);
+
+//    return function (){
+//     console.debug("return clourse:",arguments);
+//    }
+// }
+
+// createStore
+const counterStore = createStore(counterReducer, couterInitialState);
 
 // console.debug('counterReducer', counterReducer);
-// console.debug('counterStore', counterStore);
-// console.debug("counterStore.state: \n", counterStore.getState());
-const combineReducer = combineReducers({
-    counter: counterReducer,
-    todo: todoReducer
-});
+console.debug('counterStore', counterStore);
 
-const value = combineReducer({ value: 0 }, {
-    type: 'counter/incremented',
-    payload: {
-        step: 1
+// store.subscribe
+let currentValue = couterInitialState;
+
+const unsubscribe = counterStore.subscribe(()=>{
+  console.debug("监听 store ~~~");
+  let previousValue = currentValue
+  currentValue = counterStore.getState();
+
+  if (previousValue !== currentValue) {
+      console.log(
+        'Some deep nested property changed from',
+        previousValue,
+        'to',
+        currentValue
+      )
     }
 })
-console.debug('value', value);
+
+// store.getState
+console.debug("before decrement: \n", counterStore.getState());
+
+// store.dispatch
+counterStore.dispatch(counterAction.increment(188))
+console.debug("after decrement-before unsubscribe: \n", counterStore.getState());
+
+// unsubscribe
+unsubscribe();
+
+counterStore.dispatch(counterAction.increment(188))
+console.debug("after decrement-after unsubscribe: \n", counterStore.getState());
+
+
+
+// const combineReducer = combineReducers({
+//     counter: counterReducer,
+//     todo: todoReducer
+// });
+
+// const value = combineReducer({ value: 0 }, {
+//     type: 'counter/incremented',
+//     payload: {
+//         step: 1
+//     }
+// })
+// console.debug('value', value);
 // console.debug('combineReducer', combineReducer);
 // const fs = require('fs');
 
